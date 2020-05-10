9db852bae2901f5a64ba77db0dcd4cc220bd15ad
onukura <y.bibos.ishikawa@gmail.com>
2020-05-10 14:44:24 +0900

    add test

--- racoon/lib/evals/regression.py
+++ racoon/lib/evals/regression.py
@@ -1,13 +1,20 @@
 import numpy as np
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_log_error
 
 
 def mae(yhat, y):
-    return float(np.mean(np.abs(yhat - y)))
+    return mean_absolute_error(yhat, y)
 
 
 def mse(yhat, y):
-    return float(np.mean((yhat - y) ** 2))
+    return mean_squared_error(yhat, y)
+
+
+def msle(yhat, y):
+    return mean_squared_log_error(yhat, y)
 
 
 def rmse(yhat, y):
-    return float(np.std(yhat - y))
+    return np.sqrt(mse(yhat, y))
---
+++ racoon_tests/lib_tests/eval_tests/test_regression.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+import numpy as np
+
+from racoon.lib.evals.regression import mae, mse, msle, rmse
+
+
+class Test(TestCase):
+
+    def setUp(self) -> None:
+        self.answer = np.array([1, 2, 3, 4, 5])
+        self.predict = np.array([2, 2, 2, 2, 2])
+
+    def test_mae(self):
+        r = float(np.abs((self.answer - self.predict)).mean())
+        self.assertEqual(r, mae(self.answer, self.predict))
+
+    def test_mse(self):
+        r = float(np.mean((self.answer - self.predict)**2))
+        self.assertEqual(r, mse(self.answer, self.predict))
+
+    def test_msle(self):
+        r = float(np.mean((np.log1p(self.answer) - np.log1p(self.predict))**2))
+        self.assertEqual(r, msle(self.answer, self.predict))
+
+    def test_rmse(self):
+        r = float(np.sqrt(np.mean((self.answer - self.predict)**2)))
+        self.assertEqual(r, rmse(self.answer, self.predict))

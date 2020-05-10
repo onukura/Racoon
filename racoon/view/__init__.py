b9e8bb985164d856c5564bb43552eab1b56aa17e
onukura <y.bibos.ishikawa@gmail.com>
2020-05-11 01:29:08 +0900

    update docker

--- racoon/view/__init__.py
+++ racoon/view/__init__.py
@@ -1,7 +1,9 @@
 from racoon.view.index import bp_index
 from racoon.view.auth.auth import bp_auth
+from racoon.view.compete import bp_compete
 
 views = {
     "bp_index": bp_index,
     "bp_auth": bp_auth,
+    "bp_compete": bp_compete,
 }

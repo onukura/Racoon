2b4f1e7bde340b428aeb5aaecda0f0854d879c9e
onukura <y.bibos.ishikawa@gmail.com>
2020-05-18 02:46:15 +0900

    update html

--- racoon/view/compete.py
+++ racoon/view/compete.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask_login import current_user
 
 from racoon.view.auth.utils import login_or_role_erquired
 
-bp_compete = Blueprint("bp_compete", __name__)
+bp_compete = Blueprint("bp_compete", __name__, url_prefix="/compete")
 
 
-@bp_compete.route("/compete")
+@bp_compete.route("/")
 @login_or_role_erquired("member")
 def compete():
     return render_template("compete.html", user=current_user)
+
+
+@bp_compete.route("/create")
+@login_or_role_erquired("creator")
+def create():
+    return render_template("compete_create.html")

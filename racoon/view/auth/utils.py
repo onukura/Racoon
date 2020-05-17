aa49b861a8e24aead4d0a19d222a71c1cc51075b
onukura <y.bibos.ishikawa@gmail.com>
2020-05-17 14:26:02 +0900

    black

--- racoon/view/auth/utils.py
+++ racoon/view/auth/utils.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 from racoon.models.user import User, Roles
 
 
-EXEMPT_METHODS = set(['OPTIONS'])
+EXEMPT_METHODS = set(["OPTIONS"])
 
 
 def login_or_role_erquired(role_name=None):
@@ -15,7 +15,7 @@ def login_or_role_erquired(role_name=None):
         def decorated_view(*args, **kwargs):
             if request.method in EXEMPT_METHODS:
                 return func(*args, **kwargs)
-            elif current_app.config.get('LOGIN_DISABLED'):
+            elif current_app.config.get("LOGIN_DISABLED"):
                 return func(*args, **kwargs)
             elif current_user.is_authenticated:
                 if role_name is None:
@@ -23,14 +23,20 @@ def login_or_role_erquired(role_name=None):
                 else:
                     req_role = Roles.query.filter(Roles.name == role_name).first()
                     if req_role:
-                        user = User.query.filter(User.username == current_user.username).first()
+                        user = User.query.filter(
+                            User.username == current_user.username
+                        ).first()
                         if user.roles[0].id <= req_role.id:
                             return func(*args, **kwargs)
                         else:
-                            return redirect(url_for("bp_error.unauthorized", _external=True))
+                            return redirect(
+                                url_for("bp_error.unauthorized", _external=True)
+                            )
                     else:
                         ValueError("{} is invalid role name.".format(role_name))
             else:
                 current_app.login_manager.unauthorized()
+
         return decorated_view
+
     return decorator
--- racoon/view/error.py
+++ racoon/view/error.py
@@ -7,9 +7,17 @@ bp_error = Blueprint("bp_error", __name__, url_prefix="/error")
 
 @bp_error.route("/default")
 def default():
-    return render_template("error.html", header_name="Error", error_message="We will work on fixing that right away.")
+    return render_template(
+        "error.html",
+        header_name="Error",
+        error_message="We will work on fixing that right away.",
+    )
 
 
 @bp_error.route("/unauthorized")
 def unauthorized():
-    return render_template("error.html", header_name="Unauthorized", error_message="Not allowed to access this contents.")
+    return render_template(
+        "error.html",
+        header_name="Unauthorized",
+        error_message="Not allowed to access this contents.",
+    )

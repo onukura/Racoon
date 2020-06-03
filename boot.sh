4278377c4838240c80196c3d4a4b488641cc9733
onukura <y.bibos.ishikawa@gmail.com>
2020-06-04 00:54:24 +0900

    fix boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ./wait-for-it.sh postgresql:5432 -- echo 'postgres is up'
 python manage.py init_app
-waitress-serve --listen=0.0.0.0:5000 manage:app
+gunicorn -w 2 -b 0.0.0.0:5000 manage:app

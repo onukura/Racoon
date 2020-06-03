b48f564cb7698fe20e7338986c7c292c30bf34f0
onukura <y.bibos.ishikawa@gmail.com>
2020-06-04 02:05:34 +0900

    wip user profile page: home #15

--- racoon/view/__init__.py
+++ racoon/view/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from racoon.view.auth.auth import bp_auth
-from racoon.view.compete.compete import bp_compete
+from racoon.view.auth.routes import bp_auth
+from racoon.view.compete.routes import bp_compete
 from racoon.view.error.custom import bp_error
 from racoon.view.home import bp_home
+from racoon.view.user.routes import bp_user
 
 
 views = {
@@ -10,6 +11,7 @@ views = {
     "bp_error": bp_error,
     "bp_auth": bp_auth,
     "bp_compete": bp_compete,
+    "bp_user": bp_user,
 }
 
 
